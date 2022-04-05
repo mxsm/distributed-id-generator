be029a524e11702481d85218e6c784df2936577b
mxsm <ljbmxsm@gmail.com>
2022-04-05 21:28:57 +0800

    create mysql model

---
+++ generator-mysql/src/main/java/com/github/mxsm/GeneratorMysqlBootstrap.java
@@ -0,0 +1,18 @@
+package com.github.mxsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mxsm
+ * @date 2022/4/5 21:21
+ * @Since 1.0.0
+ */
+@SpringBootApplication
+public class GeneratorMysqlBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorMysqlBootstrap.class,args);
+    }
+
+}
